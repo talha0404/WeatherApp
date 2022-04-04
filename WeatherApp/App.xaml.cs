e0b38d0d380d1b8ad1734052992164775942ddb9
AHMET TALHA ŞAHİN <talha040441@hotmail.com>
2022-04-04 17:09:11 +0300

    Add project files.

---
+++ WeatherApp/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using WeatherApp.Views;
+
+namespace WeatherApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new HomePage();
+	}
+}
---
+++ WeatherApp/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace WeatherApp;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+		CounterLabel.Text = $"Current asdsa: {count}";
+
+		SemanticScreenReader.Announce(CounterLabel.Text);
+	}
+}
+
